a939e8b207f491083082051107774ff682eb9205
pranavwadatkar785-creator <pranavwadatkar785@gmail.com>
2026-08-12 18:26:52 +0530

    Dunder methods

--- OOP/baics.py
+++ OOP/baics.py
@@ -71,5 +71,50 @@ class Atm:
             print("Wrong Pin.")
         self.menu()
 
-obj = Atm()
-print(obj.pin,obj.balance)
+# obj = Atm()
+# print(obj.pin,obj.balance)
+
+class Fraction:
+
+    #parameterized constructor
+    def __init__(self,x,y):
+        self.num = x
+        self.den = y
+
+    def __str__(self):
+        return f"{self.num}/{self.den}"
+
+    def __add__(self, other):
+        new_num = self.num*other.den + other.num*self.den
+        new_den = self.den*other.den
+
+        return f"{new_num}/{new_den}"
+
+    def __sub__(self, other):
+        new_num = self.num*other.den - other.num*self.den
+        new_den = self.den*other.den
+        
+        return f"{new_num}/{new_den}"
+
+    def __mul__(self, other):
+        new_num = self.num*other.num
+        new_den = self.den*other.den
+        
+        return f"{new_num}/{new_den}"
+
+    def __truediv__(self, other):
+        new_num = self.num*other.den
+        new_den = self.den*other.num
+        
+        return f"{new_num}/{new_den}"
+
+    def convert_to_decimal(self):
+        return self.num/self.den
+
+fr1 = Fraction(1,4)
+fr2 = Fraction(1,4)
+print(fr1 + fr2)
+print(fr1 - fr2)
+print(fr1 * fr2)
+print(fr1 / fr2)
+print(fr1.convert_to_decimal())
